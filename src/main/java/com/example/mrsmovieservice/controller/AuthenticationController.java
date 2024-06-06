42dd5a09e9830d3d32920102c344c3bb32c4aa6c
MrSharma <MrSharma619@local.com>
2024-06-06 20:17:52 +0530

    register function minor fixes

--- src/main/java/com/example/mrsmovieservice/controller/AuthenticationController.java
+++ src/main/java/com/example/mrsmovieservice/controller/AuthenticationController.java
@@ -3,10 +3,12 @@ package com.example.mrsmovieservice.controller;
 import com.example.mrsmovieservice.dto.LoginResponse;
 import com.example.mrsmovieservice.dto.LoginUserDto;
 import com.example.mrsmovieservice.dto.RegisterUserDto;
+import com.example.mrsmovieservice.dto.UserResponse;
 import com.example.mrsmovieservice.entity.User;
 import com.example.mrsmovieservice.service.AuthenticationService;
 import com.example.mrsmovieservice.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,10 +24,17 @@ public class AuthenticationController {
     private AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+    public ResponseEntity<UserResponse> register(@RequestBody RegisterUserDto registerUserDto) {
         User registeredUser = authenticationService.register(registerUserDto);
 
-        return ResponseEntity.ok(registeredUser);
+        UserResponse userResponse = new UserResponse(
+                registeredUser.getId(),
+                registeredUser.getFullName(),
+                registeredUser.getEmail(),
+                registeredUser.getPlatformUserName()
+        );
+
+        return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
